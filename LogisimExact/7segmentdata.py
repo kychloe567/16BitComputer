5a6fa287d35edf76fd685acfe2f256c1ed047d9e
kurinczkydd@stud.uni-obuda.hu <kurinczkydd@stud.uni-obuda.hu>
2024-01-19 12:33:38 +0100

    7SEGMENT working

--- LogisimExact/7segmentdata.py
+++ LogisimExact/7segmentdata.py
@@ -28,7 +28,6 @@ def getBinaryFor7Seg(d):
 
 digits = {key: sort_and_fill(value, positions) for key, value in digits.items()}
 
-#10110011
 toFileLeft = ""
 toFileRight = ""
 lastPerc = -1
@@ -38,116 +37,49 @@ with open("7SEG_LEFT.txt","w") as f:
 with open("7SEG_RIGHT.txt","w") as f:
     f.write("")
 
-for data in itertools.product([0,1], repeat=8):
-    binary_string = ''.join(map(str, data))
-    integer = int(binary_string, 2)
-    max_ = int("1"*8,2)
-    perc = math.floor((integer/max_)*100)
-    if perc != lastPerc:
-        #print(perc)
-        lastPerc = perc
-        #if perc % 5 == 0:
-        #    with open("7SEG_LEFT.txt","a") as f:
-        #        f.write(toFileLeft)
-        #    with open("7SEG_RIGHT.txt","a") as f:
-        #        f.write(toFileRight)
-        #    toFileLeft = ""
-        #    toFileRight = ""
 
-    #string = hex(integer)[2:].zfill(2)
-    #print(string)
-    for GND in itertools.product([0,1], repeat=3):
-        GND_string = "000"
-        for TWOS in range(2):
-            TWOS_string = str(TWOS)
-            for CLK in range(2):
-                CLK_string = str(CLK)
+dec = 0
+for CLK in range(2):
+    CLK_string = str(CLK)
+    for TWOS in range(2):
+        TWOS_string = str(TWOS)
+        for GND in itertools.product([0,1], repeat=3):
+            GND_string = "000"
+
+            for data in itertools.product([0,1], repeat=8):
+                binary_string = ''.join(map(str, data))
+                integer = int(binary_string, 2)
+                max_ = int("1"*8,2)
+                perc = math.floor((integer/max_)*100)
+                if perc != lastPerc:
+                    lastPerc = perc
+
                 leftString = hex(int(binary_string + "00000000",2))[2:].zfill(4)
                 rightString = hex(int(binary_string,2))[2:].zfill(4)
                 if CLK == 0:
-                    left = leftString[0]
-                    right = rightString[2]
-                    hexLeft = format(int(getBinaryFor7Seg(left), 2), '02X')
-                    hexRight = format(int(getBinaryFor7Seg(right), 2), '02X')
-                if CLK == 1:
                     left = leftString[1]
                     right = rightString[3]
-                    hexLeft = format(int(getBinaryFor7Seg(left), 2), '02X')
-                    hexRight = format(int(getBinaryFor7Seg(right), 2), '02X')
-                #print(hexRight + "\t" + right + "\t" + getBinaryFor7Seg(right))
+                if CLK == 1:
+                    left = leftString[0]
+                    right = rightString[2]
+
+                hexLeft = format(int(getBinaryFor7Seg(left), 2), '02X')
+                hexRight = format(int(getBinaryFor7Seg(right), 2), '02X')
+
+                #print(str(dec) + ": \t" + hexRight + "\t" + rightString + "\t" + CLK_string)
+
                 toFileLeft += hexLeft + " "
                 toFileRight += hexRight + " "
-                #print(hexRight)
-                
+                dec += 1
+
+
+
 
 with open("7SEG_LEFT.txt","w") as f:
     f.write(toFileLeft)
 with open("7SEG_RIGHT.txt","w") as f:
     f.write(toFileRight)
 
-"""
-for i in itertools.product([0,1], repeat=16):
-    binary_string = ''.join(map(str, i))
-    integer = int(binary_string, 2)
-    string = hex(integer)[2:]
-    while len(string) < 4:
-        string = " " + string
-    
-    toDigits = []
-
-    value = ""
-    for c in string:
-        if c == " ":
-            value = "00000000" + value
-            continue
-        digit = digits[c]
-        bitValue = ""
-        for d in digit:
-            if d == "x":
-                bitValue = "0" + bitValue
-            else:
-                bitValue = "1" + bitValue
-        value = bitValue + value
-
-    hexadecimal_value = format(int(value, 2), '08X')
-    toFile += hexadecimal_value + " "
-"""
-"""
-for i in itertools.product([0,1], repeat=16):
-    binary_string = ''.join(map(str, i))
-    integer = int(binary_string, 2) * -1
-    print(integer)
-    string = hex(integer)
-    print(string)
-    while len(string) < 4:
-        string = " " + string
-    
-    toDigits = []
-    
-    value = ""
-    for c in string:
-        if c == " ":
-            value = value + "00000000"
-            continue
-        digit = digits[c]
-        for d in digit:
-            if d == "x":
-                value = value + "0"
-            else:
-                value = value + "1"
-
-    hexadecimal_value = format(int(value, 2), '08X')
-    toFile += hexadecimal_value + " "
-    
-    
-    #['', 'abdeg', 'abcdeg', 'acdefg']
-    #bafg
-    #pcde
-    
-"""
-"""
-with open("hexadata.txt","w") as f:
-    f.write(toFile)
 
 print("Done!")
-"""
+
